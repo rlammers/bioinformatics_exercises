f086d353883ab5645288c3f2cd81ea160823d32b
Roger Lammers <rmlammers@gmail.com>
2014-12-11 16:19:39 +1300

    Solved prob

--- prob/prob.py
+++ prob/prob.py
@@ -2,8 +2,11 @@ import sys
 import math
 
 def main(argv):
+	print("begin")
 	if len(argv) == 2:
-		readInputFile(argv[0])
+		dna_and_probs = readInputFile(argv[0])
+		logprobs = calculateProbs(dna_and_probs)
+		writeProbsToFile(logprobs, argv[1])
 	else:
 		usage()
 		sys.exit(2)
@@ -12,18 +15,34 @@ def readInputFile(filename):
 	with open(filename) as f:
 		dna_string = f.readline().rstrip()
 		probs = f.readline().split(' ')
-		for prob in probs:
-			gc_prob = float(prob)/2
-			at_prob = (1-float(prob))/2
-			result = math.log(gc_prob**countGC(dna_string)*at_prob**countAT(dna_string), 10)
-			print(result)
+		return [dna_string, probs]
 
+def countGCAT(dna_string):
+	gc = dna_string.count('G') + dna_string.count('C')
+	at = dna_string.count('A') + dna_string.count('T')
+	return [gc, at]
 
-def countGC(dna_string):
-	return dna_string.count('G') + dna_string.count('C')
+def calculateProbs(dna_and_probs):
+	dna_string = dna_and_probs[0]
+	probs = dna_and_probs[1]
 
-def countAT(dna_string):
-	return dna_string.count('A') + dna_string.count('T')
+	gcat_counts = countGCAT(dna_string)
+	gc_count = gcat_counts[0]
+	at_count = gcat_counts[1]
+
+	results = []
+	for prob in probs:
+		gc_prob = float(prob)/2
+		at_prob = (1-float(prob))/2
+		gc_result = gc_prob**gc_count
+		at_result = at_prob**at_count
+		result = math.log(gc_result * at_result, 10)
+		results.append(result)
+	return results
+
+def writeProbsToFile(results, outputfile):
+	with open(outputfile, 'w') as f:
+		f.write(" ".join(format(res, "1.3f") for res in results))
 
 def usage():
 	print("USAGE: prob.py inputfile outputfile")
