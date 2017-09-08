b953e86b843eebbec0f4ee4f20ca3a4b076c8520
Roger Lammers <rmlammers@gmail.com>
2017-09-08 19:04:36 +1200

    Fixed bug with copying references not values

--- perm/perm.py
+++ perm/perm.py
@@ -1,3 +1,5 @@
+import copy
+
 def main():
     # We need to get the length of the permutation from file
     with open('sample.in', 'r') as f:
@@ -9,18 +11,19 @@ def main():
         initial_perm.append(i+1)
     all_perms = generate_perms(perm_len, initial_perm)
     print(all_perms)
+    # format_and_print(all_perms)
 
 
 # Heap's algorithm non-recursive
 def generate_perms(n, perm_list):
-    c = dict()
+    c = {}
 
     i = 0
     while i < n:
         c[i] = 0
         i += 1
 
-    all_perms = [perm_list]
+    all_perms = [copy.deepcopy(perm_list)]
 
     i = 0
     while i < n:
@@ -29,13 +32,24 @@ def generate_perms(n, perm_list):
                 perm_list[0], perm_list[i] = perm_list[i], perm_list[0]
             else:
                 perm_list[c[i]], perm_list[i] = perm_list[i], perm_list[c[i]]
-            all_perms.append(perm_list)
+
+            # Add values not references
+            all_perms.append(copy.deepcopy(perm_list))
+
             c[i] += 1
             i = 0
         else:
             c[i] = 0
             i += 1
     return all_perms
+
+
+def format_and_print(perms):
+    print(len(perms))
+    for x in perms:
+        for y in x:
+            print(str(y) + ' ', end='')
+        print()
 
 
 if __name__ == '__main__':
